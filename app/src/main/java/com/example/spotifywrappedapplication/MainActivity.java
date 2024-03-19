7e8e61d1c024b56956e73c844f8384579a67de1c
PhillipPhanhthourath <phanhthourath.phillip@gmail.com>
2024-03-19 11:44:59 -0500

    fine-tuned API setup

--- app/src/main/java/com/example/spotifywrappedapplication/MainActivity.java
+++ app/src/main/java/com/example/spotifywrappedapplication/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String REDIRECT_URI = "spotifywrappedapplication://auth";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
